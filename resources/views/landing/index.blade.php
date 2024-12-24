8956cc8ff3e515bd906a646e5dbf170c3e7d675b
Metin Ahmad <metin.ahmad@paligo.net>
2024-12-24 20:41:19 +0100

    Use postcss tailwind rather than cdn

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catherine Conelly</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
 </head>
 <body class="min-h-screen flex bg-gray-50 text-gray-800">
     <!-- Sidebar -->
--- resources/views/library/index.blade.php
+++ resources/views/library/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
 </head>
 
 <body class="min-h-screen flex bg-gray-50 text-gray-800">
