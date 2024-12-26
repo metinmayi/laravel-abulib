4de6f167a525e1a45e7e8c6468332b440fb84f3a
Metin Ahmad <metin.ahmad@paligo.net>
2024-12-26 17:56:02 +0100

    Removed admin section from sidebar and instead conditionally render admin options.

--- resources/views/components/siderbar.blade.php
+++ resources/views/components/siderbar.blade.php
@@ -7,15 +7,10 @@
                 <li><a href="#about" class="text-lg hover:text-gray-500">About Abdulbaghi</a></li>
                 <li><a href="/library" class="text-lg hover:text-gray-500">Library</a></li>
                 <li><a href="/login" class="text-lg hover:text-gray-500">Login</a></li>
-                <li><a href="/admin" class="text-lg hover:text-gray-500">Admin</a></li>
-            </ul>
-
-            @auth
-            <h1 class="mt-10 text-3xl">Admin Panel</h1>
-            <ul class="space-y-4">
+                @auth
                 <li><a href="/admin/newliterature" class="text-lg hover:text-gray-500">Upload literature</a></li>
-                <li><a href="/admin/newvariant" class="text-lg hover:text-gray-500">Upload Language Variant</a></li>
+                @endauth
+                    
             </ul>
-            @endauth
         </nav>
     </div>
