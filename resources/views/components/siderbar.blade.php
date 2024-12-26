782ed2063c0f254167eb477930899b16f264aaeb
Metin Ahmad <metin.ahmad@paligo.net>
2024-12-26 14:33:29 +0100

    Moved admin panel to sidebar if logged in

--- resources/views/components/siderbar.blade.php
+++ resources/views/components/siderbar.blade.php
@@ -7,6 +7,15 @@
                 <li><a href="#about" class="text-lg hover:text-gray-500">About Abdulbaghi</a></li>
                 <li><a href="/library" class="text-lg hover:text-gray-500">Library</a></li>
                 <li><a href="/login" class="text-lg hover:text-gray-500">Login</a></li>
+                <li><a href="/admin" class="text-lg hover:text-gray-500">Admin</a></li>
             </ul>
+
+            @auth
+            <h1 class="mt-10 text-3xl">Admin Panel</h1>
+            <ul class="space-y-4">
+                <li><a href="/admin/newliterature" class="text-lg hover:text-gray-500">Upload literature</a></li>
+                <li><a href="/admin/add-language" class="text-lg hover:text-gray-500">Upload Language Variant</a></li>
+            </ul>
+            @endauth
         </nav>
     </div>
