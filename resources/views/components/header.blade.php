455086b92e590e542ed50395e865d801a238b8da
Metin Ahmad <metin.ahmad@paligo.net>
2025-01-06 10:52:59 +0100

    Attempt att fixing issue with flags not rendering on initial mobile load

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -816,9 +816,9 @@
     <div class="cs-container">
         <div class="cs-logo gap-2">
             @foreach (\App\Models\Literature::LANGUAGES as $lang)
-                <a href="{{ route('locale.change', ['locale' => $lang]) }}" class="group relative">
+                <a href="{{ route('locale.change', ['locale' => $lang]) }}">
                     <img src="{{ asset("images/$lang-flag.svg") }}" alt="{{ $lang }} flag" loading="eager"
-                        class="w-8 h-8 border border-gray-300 rounded shadow-sm hover:scale-110 transition-transform duration-200">
+                        class="w-8 h-8 border border-gray-300 rounded shadow-sm">
                     {{-- <span
                         class="absolute -bottom-6 left-1/2 transform -translate-x-1/2 scale-0 group-hover:scale-100 bg-gray-800 text-white text-xs rounded px-2 py-1 transition-transform duration-200">
                         {{ __(ucfirst($lang)) }}
