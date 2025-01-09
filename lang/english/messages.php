22d7e8ba5a8a5ab5e84c350b6caa9a2373445546
Metin Ahmad <metin.ahmad@paligo.net>
2025-01-09 21:45:40 +0100

    Add locale

--- lang/english/messages.php
+++ lang/english/messages.php
@@ -12,6 +12,7 @@ return [
   'Swedish' => 'Swedish',
   'Kurdish' => 'Kurdish',
   'Arabic' => 'Arabic',
+  'Read' => 'Read',
   'no-pdf' => 'No PDF available for this language. Please select another language from the list above.',
   'landing-bio' => 'Abdulbaghi Ahmad (Mayi) has his expertise in child mental health and child and adolescent psychiatry with focus on childhood trauma and posttraumatic health and wellbeing. His open and contextual creative concept based on a bio-psycho-social model of trauma and posttraumatic growth creating new pathways for improving the immunity and child mental health. He has developed several innovations and constructions for investigation, prevention and treatment of child mental health problems after years of clinical experience, research, evaluation, teaching and administration both in hospital and education institutions to provide suitable environment for the child for building up a healthy personality and prosperous society.',
   'landing-title' => 'A SWEDISH PHYSICIAN FROM KURDISTAN',
--- lang/swedish/messages.php
+++ lang/swedish/messages.php
@@ -12,6 +12,7 @@ return [
     'Swedish' => 'Svenska',
     'Kurdish' => 'Kurdiska',
     'Arabic' => 'Arabiska',
+    'Read' => 'Läs',
     'no-pdf' => 'Ingen PDF tillgänglig för detta språk. Vänligen välj ett annat språk från listan ovan.',
     'landing-bio' => 'Abdulbaghi Ahmad (Mayi) har sin expertis inom barns psykiska hälsa och barn- och ungdomspsykiatri med fokus på barndomstrauma och posttraumatisk hälsa och välbefinnande. Hans öppna och kontextuella kreativa koncept baserat på en bio-psyko-social modell av trauma och posttraumatisk tillväxt skapar nya vägar för att förbättra immunitet och barns mentala hälsa. Han har utvecklat flera innovationer och konstruktioner för utredning, förebyggande och behandling av barns psykiska problem efter år av klinisk erfarenhet, forskning, utvärdering, undervisning och administration både på sjukhus och utbildningsinstitutioner för att tillhandahålla lämplig miljö för barnet för att bygga upp en hälsosam personlighet och välmående samhälle.',
     'landing-title' => 'En svensk läkare från Kurdistan',
